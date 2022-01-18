ea0e0e50f8790f9d9bff8a5f3bc8014ebe422d6f
Cian Butler <ciandevelops@gmail.com>
2022-01-18 12:19:29 -0500

    navbar complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,13 @@ import reactLogo from "../images/logo.png";
 
 const Header = () => {
   return (
-    <nav>
-      <div>
+    <nav className="nav">
+      <div className="left-nav">
         <img src={reactLogo} alt="logo" />
+        <h3 className="logo-text">ReactFacts</h3>
       </div>
 
-      <div>
-        <h3>React Course - Project 1</h3>
-      </div>
+      <h4 className="title-text">React Course - Project 1</h4>
     </nav>
   );
 };
